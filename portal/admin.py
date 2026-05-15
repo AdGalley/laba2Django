8d6bb3c59e8ff9d0f4702a60f022fe58cbdb667c
AdGalley <teeftel.333@gmail.com>
2026-05-15 23:31:08 +0700

    2.2 регистрация моделей в админке

--- portal/admin.py
+++ portal/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import CustomUser, Category, Application
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'last_name', 'first_name', 'middle_name', 'email')
+    search_fields = ('username', 'email', 'last_name', 'first_name')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'category', 'status', 'created_at')
+    list_filter = ('status', 'category', 'created_at')
+    search_fields = ('title', 'description', 'user__username')
+    readonly_fields = ('created_at',)
