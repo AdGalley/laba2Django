1dbab0907d8fc1b01372c9d907bc5f1834ab7d79
AdGalley <teeftel.333@gmail.com>
2026-05-16 01:40:38 +0700

    2.2 добавил функционала заявкам, пока что не отображаются на главной, так же теперь работает через localhost8089

--- portal/urls.py
+++ portal/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('create-application/', views.create_application, name='create_application'),
+    path('my-applications/', views.my_applications, name='my_applications'),
+    path('delete-application/<int:pk>/', views.delete_application, name='delete_application'),
 ]
